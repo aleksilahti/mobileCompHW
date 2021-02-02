f0616eb25005c6b3ed6e9f3a1cd6d8ea01955c48
aleksilahti <allu_lahti@hotmail.com>
2021-02-02 17:13:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mobilecomphw/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.mobilecomphw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.login_button).setOnClickListener {
+            Log.d()
+        }
+    }
+}
